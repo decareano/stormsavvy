79b18f420a1ad5c1a383e34be5d29feca65e5fd5
Walter Yu <kharma@gmail.com>
2012-11-16 07:51:31 -0800

    add heroku ping script

---
+++ script/heroku/ping_domains.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Bash script to ping heroku subdomains.
+
+host stormsavvy.com
+host www.stormsavvy.com
+host pdf.stormsavvy.com
+host weather.stormsavvy.com
+
+ping stormsavvy.com
+ping www.stormsavvy.com
+ping pdf.stormsavvy.com
+ping weather.stormsavvy.com
